cc81b974d429cf86fc04d5457b64a0a3423e5f57
Muhammad Wahab <wahabmukhtar7@gmail.com>
2024-09-13 12:15:25 +0500

    Fix publicPath for GitHub Pages

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,13 @@
 const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
+module.exports = {
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/coinha/'  // This is your GitHub repository name
+    : '/'
+}
+
+
+
+// module.exports = defineConfig({
+//   transpileDependencies: true
+// })
+
