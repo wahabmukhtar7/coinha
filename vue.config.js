f9a0fae2641f62ed035d6feb18d720b1afa793e2
wahabmukhtar7 <85012608+wahabmukhtar7@users.noreply.github.com>
2024-09-19 05:28:09 +0000

    Update deployment

--- vue.config.js
+++ vue.config.js
@@ -1,13 +1,6 @@
-const { defineConfig } = require('@vue/cli-service')
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
     ? '/coinha/'  // This is your GitHub repository name
-    : '/'
-}
-
-
-
-// module.exports = defineConfig({
-//   transpileDependencies: true
-// })
-
+    : '/',
+  transpileDependencies: []  // This should be an array
+};
